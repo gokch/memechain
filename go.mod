bb665b4d88633830cffd81bf04e7af8db90c956b
김창학 <sjc02183@naver.com>
2024-01-21 15:16:09 +0900

    make test for zstd TODO : benchmark

--- go.mod
+++ go.mod
@@ -3,15 +3,23 @@ module github.com/gokch/memechain
 go 1.20
 
 require (
+	github.com/funkygao/assert v0.0.0-20160929004900-4a267e33bc79
 	github.com/google/uuid v1.5.0
 	github.com/rs/zerolog v1.31.0
 	github.com/spf13/cobra v1.8.0
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
---
+++ utilx/zstd_test.go
@@ -0,0 +1,28 @@
+package utilx
+
+import (
+	"testing"
+
+	"github.com/funkygao/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestZSTDCompressDecompress(t *testing.T) {
+	for _, test := range []struct {
+		name             string
+		compressionLevel int
+		origin           []byte
+	}{
+		{"empty", 1, []byte{}},
+		{"short", 2, []byte("hello world")},
+		{"long", 3, []byte("hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world")},
+	} {
+		dst := make([]byte, 0)
+		compress, err := ZSTDCompress(dst, test.origin, test.compressionLevel)
+		require.NoErrorf(t, err, "test %s", test.name)
+		recover, err := ZSTDDecompress(dst, compress)
+		require.NoErrorf(t, err, "test %s", test.name)
+		assert.Equalf(t, test.origin, recover, "test %s", test.name)
+	}
+
+}
